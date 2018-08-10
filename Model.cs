19d352cfa3f3aa16afc4f8ce48be315d9a7c4728
Disa <dis@mail.rotf.pl>
2018-08-10 02:20:26 +0200

    init

---
+++ Model.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Media.Imaging;
+using System.Windows.Media;
+
+namespace catamorphism
+{
+    class MiniList
+    {
+        public BitmapImage _icon;
+        public string _shortwebpagename;
+        public string _login;
+        public BitmapImage Icon
+        {
+            get { return _icon;  }
+        }
+        public string ShortName
+        {
+            get { return _shortwebpagename; }
+        }
+        public string Login
+        {
+            get { return _login; }
+        }
+    }
+    class PageData
+    {
+        public string _website;
+        public string _user;
+        private string _password;
+        public string _email;
+        public string _created;
+        private int _strength;
+        public string WebName
+        {
+            get { return _website; }
+        }
+        public string User
+        {
+            get { return _user; }
+        }
+        public string Password
+        {
+            get { return "●●●●●●●●●●"; }
+            set { this._password = value;
+                this._strength = (int)(UIHelper.CheckStrength(value) * 12.5d);
+            }
+        }
+        public string EMail
+        {
+            get { return _email; }
+        }
+        public string Created
+        {
+            get { return _created; }
+        }
+        public int Strength
+        {
+           get { return _strength ; }
+        }
+        public SolidColorBrush SColor
+        {
+            get {
+                SolidColorBrush br = new SolidColorBrush();
+                if (_strength > 80)
+                {
+                    br.Color = System.Windows.Media.Color.FromArgb(255, 0, 255, 0);
+                } else if(_strength > 50)
+                {
+                    br.Color = System.Windows.Media.Color.FromArgb(255, 255, 255, 0);
+                } else
+                {
+                    br.Color = System.Windows.Media.Color.FromArgb(255,255, 0, 0);
+                }
+                return br;
+           }
+        }
+    }
+    class ViewModel
+    {
+        public List<MiniList> savedpasswords;
+        public PageData pd;
+        public ViewModel()
+        {
+            pd = new PageData();
+            pd._website = "http://webtesT.com";
+            pd._user = "useeer";
+            pd.Password = "aaa123Daeg*^#";
+            pd._email = "use@mail";
+            pd._created = "01:01:2000";
+
+            savedpasswords = new List<MiniList>();
+            MiniList item = new MiniList();
+            item._login = "Test";
+            item._shortwebpagename = "Short";
+            item._icon = new BitmapImage(new Uri("C:\\Users\\Karol\\Documents\\Visual Studio 2013\\Projects\\DTun\\_0.ico"));
+            savedpasswords.Add(item);
+        }
+        public PageData Page
+        {
+            get { return pd; }
+        }
+    }
+}
---
+++ UIhelpers.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Globalization;
+using System.Windows.Media;
+using System.Text.RegularExpressions;
+
+namespace catamorphism
+{
+    static class UIHelper
+    {
+        public static int CheckStrength(string password)//https://stackoverflow.com/a/10966297
+        {
+            int score = 0;
+
+            // using three requirements here:  min length and two types of characters (numbers and letters)
+            bool blnMinLengthRequirementMet = false;
+            bool blnRequirement1Met = false;
+            bool blnRequirement2Met = false;
+
+            // check for chars in password
+            if (password.Length < 1)
+                return 0;
+
+            // if less than 6 chars, return as too short, else, plus one
+            if (password.Length < 6)
+            {
+                return 1;
+            }
+            else
+            {
+                score++;
+                blnMinLengthRequirementMet = true;
+            }
+
+            // if 8 or more chars, plus one
+            if (password.Length >= 8)
+                score++;
+
+            // if 10 or more chars, plus one
+            if (password.Length >= 10)
+                score++;
+
+            // if password has a number, plus one
+            if (Regex.IsMatch(password, @"[\d]", RegexOptions.ECMAScript))
+            {
+                score++;
+                blnRequirement1Met = true;
+            }
+
+            // if password has lower case letter, plus one
+            if (Regex.IsMatch(password, @"[a-z]", RegexOptions.ECMAScript))
+            {
+                score++;
+                blnRequirement2Met = true;
+            }
+
+            // if password has upper case letter, plus one
+            if (Regex.IsMatch(password, @"[A-Z]", RegexOptions.ECMAScript))
+            {
+                score++;
+                blnRequirement2Met = true;
+            }
+
+            // if password has a special character, plus one
+            if (Regex.IsMatch(password, @"[~`!@#$%\^\&\*\(\)\-_\+=\[\{\]\}\|\\;:'\""<\,>\.\?\/£]", RegexOptions.ECMAScript))
+                score++;
+
+            // if password is longer than 2 characters and has 3 repeating characters, minus one (to minimum of score of 3)
+            List<char> lstPass = password.ToList();
+            if (lstPass.Count >= 3)
+            {
+                for (int i = 2; i < lstPass.Count; i++)
+                {
+                    char charCurrent = lstPass[i];
+                    if (charCurrent == lstPass[i - 1] && charCurrent == lstPass[i - 2] && score >= 4)
+                    {
+                        score++;
+                    }
+                }
+            }
+            if (!blnMinLengthRequirementMet || !blnRequirement1Met || !blnRequirement2Met)
+            {
+                return 2;
+            }
+            return score;
+        }
+
+    }
+
+}
